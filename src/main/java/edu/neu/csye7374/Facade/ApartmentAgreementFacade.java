4e9f60f35fbcec88dab1ec289ba0b797e60c7424
Harsha0235 <114704562+Harsha0235@users.noreply.github.com>
2024-12-09 12:20:48 -0500

    Facade Implementation
    Implement ApartmentAgreementFacade for streamlined apartment agreements

---
+++ src/main/java/edu/neu/csye7374/Facade/ApartmentAgreementFacade.java
@@ -0,0 +1,37 @@
+package edu.neu.csye7374.Facade;
+
+import edu.neu.csye7374.ApartmentAPI;
+import edu.neu.csye7374.Decorator.ApartmentDecorator;
+import edu.neu.csye7374.Decorator.Insurance;
+import edu.neu.csye7374.Observer.ApartmentAgreement;
+
+public class ApartmentAgreementFacade {
+	private ApartmentAPI apartment;
+
+	public ApartmentAgreementFacade() {
+		super();
+	}
+
+	public ApartmentAgreementFacade(ApartmentAPI apartment) {
+		super();
+		this.setApartment(apartment);
+	}
+
+	public ApartmentAgreementFacade setApartment(ApartmentAPI apartment) {
+		this.apartment = apartment;
+		return this;
+	}
+
+	public ApartmentAPI getApartment() {
+		return apartment;
+	}
+
+	public ApartmentAgreement apartmentAgreement() {
+            ApartmentAgreement agreement = new ApartmentAgreement();
+            ApartmentDecorator insurance = new Insurance(apartment);
+            System.out.println(insurance);
+            agreement.addApartment(insurance);
+            return agreement;
+	}
+
+}
